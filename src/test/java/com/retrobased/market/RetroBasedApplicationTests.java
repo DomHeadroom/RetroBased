8e0fa9835db177df2602e25c66db031ea66bce5d
DomHeadroom <domefemia@protonmail.com>
2024-05-03 16:59:40 +0200

    Commit iniziali

---
+++ src/test/java/com/retrobased/market/RetroBasedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retrobased.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroBasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
