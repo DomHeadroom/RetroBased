1a8e0bc4d86956ea9a3f8c826137029c40b452f0
Domenico <50798132+DomHeadroom@users.noreply.github.com>
2024-05-15 12:15:35 +0200

    Aggiunta eccezione e altre robe

--- src/main/java/com/retrobased/market/repositories/RepositoryOggettoCarrello.java
+++ src/main/java/com/retrobased/market/repositories/RepositoryOggettoCarrello.java
@@ -1,10 +1,19 @@
 package com.retrobased.market.repositories;
 
+import com.retrobased.market.entities.OggettoCarrello;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+
 
 
 @Repository
-public interface RepositoryOggettoCarrello {
-    public Integer getQuantity(Integer id);
+public interface RepositoryOggettoCarrello extends JpaRepository<OggettoCarrello, Integer> {
+    Integer getQuantity(Integer id);
+
+    boolean existsById(Integer id);
+
+    @Query("FROM ") // TODO Query da fare per vedere se esiste l'oggetto nel carrello
+    boolean existsInCart(Integer idCart, Integer idObject);
 
 }
--- src/main/java/com/retrobased/market/services/OggettoCarrelloService.java
+++ src/main/java/com/retrobased/market/services/OggettoCarrelloService.java
@@ -1,6 +1,7 @@
 package com.retrobased.market.services;
 
 import com.retrobased.market.repositories.RepositoryOggettoCarrello;
+import com.retrobased.market.support.exceptions.ArgumentValueNotValid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +10,15 @@ public class OggettoCarrelloService {
     @Autowired
     private RepositoryOggettoCarrello repoCart;
 
-    public void updateValueObject(Integer id, Integer value){
-        if(value.equals(Integer.MAX_VALUE) || value.equals(Integer.MIN_VALUE)){
+    public void updateValueObject(Integer idCart, Integer idObject, Integer value) throws ArgumentValueNotValid {
+        if(value.equals(Integer.MAX_VALUE) || value.equals(Integer.MIN_VALUE))
             throw new ArgumentValueNotValid();
-        }
+        if(!repoCart.existsById(idCart))
+            throw new CartNotExist();
+        if(!repoCart.existsInCart(idCart,idObject))
+            throw new ObjectNotExistInCart();
+
+
     }
 
 }
