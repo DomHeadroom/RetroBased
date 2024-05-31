46fac11a49cfc2d1c80fea86c4b1c1e54ff9ea26
DomHeadroom <domefemia@protonmail.com>
2024-05-31 10:53:28 +0200

    Rimossi check null, continuo autenticazione, aggiunte librerie

--- src/backend/main/java/com/retrobased/market/support/authentication/JwtService.java
+++ src/backend/main/java/com/retrobased/market/support/authentication/JwtService.java
@@ -2,19 +2,52 @@ package com.retrobased.market.support.authentication;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.lang.Function;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
 
     private static final String SECRET = "425a4053752e244c216a4447752f2b67595623642b26794451273d2730676f48293b54682a7e4d2b572a2e32654f5c49663948635b7b524b456b2226215b61";
 
-    public String extractUserID(String token) {
-        return null;
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(
+            Map<String, Object> claims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String
     }
 
     private Claims extractAllClaims(String token) {
