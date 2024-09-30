74823bb4dbe629b171655ae630ddcae7cc3ec7e6
DomHeadroom <domefemia@protonmail.com>
2024-09-30 13:58:47 +0200

    Creata annotazione PhoneNumber e aggiunta a seller

--- src/backend/main/java/com/retrobased/market/dtos/SellerDTO.java
+++ src/backend/main/java/com/retrobased/market/dtos/SellerDTO.java
@@ -3,6 +3,7 @@ package com.retrobased.market.dtos;
 import com.retrobased.market.entities.Country;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import validators.ValidPhoneNumber;
 
 public record SellerDTO(
 
@@ -11,7 +12,8 @@ public record SellerDTO(
 
         String company,
 
-        @NotBlank
+        @NotBlank(message = "Phone number cannot be empty.")
+        @ValidPhoneNumber
         String phoneNumber,
 
         @NotBlank(message = "Address line 1 cannot be empty.")
---
+++ src/backend/main/java/validators/PhoneNumberValidator.java
@@ -0,0 +1,19 @@
+package validators;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PhoneNumberValidator implements ConstraintValidator<ValidPhoneNumber, String> {
+
+    @Override
+    public void initialize(ValidPhoneNumber constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String phoneNumber, ConstraintValidatorContext context) {
+        if (phoneNumber == null || phoneNumber.isEmpty())
+            return true;
+
+        return phoneNumber.matches("^\\+?[0-9. ()-]{7,25}$");
+    }
+}
