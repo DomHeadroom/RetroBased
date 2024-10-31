c73cec88a0c73b830e25ab7df35b65f620b92de1
DomHeadroom <domefemia@protonmail.com>
2024-10-31 17:13:31 +0100

    Creato componente card, setting vscode per java null annotation

---
+++ Frontend/src/app/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+  title: string = "";
+  seller: string = "";
+  price: number = 0;
+}
