25f7111f81438eea038c36e57d343955d9e7352a
DomHeadroom <domefemia@protonmail.com>
2024-11-01 12:40:19 +0100

    Aggiustato componente notification, aggiunto codice notification

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskbarComponent } from './taskbar/taskbar.component'
+import { WindowComponent } from './window/window.component'
+import { NotificationComponent } from "./notification/notification.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TaskbarComponent],
+  imports: [RouterOutlet, TaskbarComponent, WindowComponent, NotificationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- Frontend/src/app/notification/notification.component.ts
+++ Frontend/src/app/notification/notification.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-notification',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './notification.component.html',
   styleUrl: './notification.component.scss'
 })
 export class NotificationComponent {
   header: string = '';
   notificationText: string = '';
+  fadeClass: boolean = false;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.fadeClass = true;
+    }, 5000);
+  }
 }
