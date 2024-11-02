70ddab4554e6b5722da88a4e02054f3d190fda86
DomHeadroom <domefemia@protonmail.com>
2024-11-02 17:52:30 +0100

    Inizio setup api con backend

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { ApiModule } from './services/api.module';
+import { API_BASE_PATH } from './app.tokens';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([])),
+    ApiModule,
+    { provide: API_BASE_PATH, useValue: 'http://localhost:8081/api' }
+  ]
 };
