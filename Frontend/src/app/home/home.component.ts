5be4eb36684a986266031ee7c959d2f9618b8cd4
DomHeadroom <domefemia@protonmail.com>
2024-11-17 21:18:54 +0100

    Cambiato immagini start, rimossa dipendenza node a 7.css, rework searchbar nella taskbar, inizio chiamate api

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,10 +1,14 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskbarComponent } from '../taskbar/taskbar.component'
 import { WindowComponent } from '../window/window.component'
 import { NotificationComponent } from '../notification/notification.component';
 import { CardComponent } from '../card/card.component';
+
 import { NotificationService } from '../services/services/notification.service';
+import { ProductControllerService } from '../services/services';
+import { Observable } from 'rxjs';
+import { ProductDto } from '../services/models';
 
 @Component({
   selector: 'app-home',
@@ -18,7 +22,15 @@ export class HomeComponent {
   notificationHeader: string = 'Login';
   notificationText: string = 'If you want to login, click here.';
 
-  constructor(private notificationService: NotificationService) {}
+  products$: Observable<any> | undefined;
+
+  constructor(
+    private notificationService: NotificationService,
+    private productService: ProductControllerService
+  ) {
+    this.products$ = this.productService.searchProducts({ k: 'sample product', page: 0 });
+    console.log(this.products$);
+  }
 
   ngOnInit() {
     this.notificationService.showNotification(this.notificationHeader, this.notificationText, 92.200);
