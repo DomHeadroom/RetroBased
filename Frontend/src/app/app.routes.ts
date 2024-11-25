6af088a66452e2ce0727795536bec3aa2b3f338e
DomHeadroom <domefemia@protonmail.com>
2024-11-25 12:29:29 +0100

    Aggiunte ico carrello, aggiunta route carrello, fixxato scss e src ico html

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
 
 /*import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';*/
 
 export const routes: Routes = [
-    {path: 'home', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'bin', component: CartComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' }
-  /*{ path: 'bin', component: HomeComponent },
+  /*
   { path: 'about', component: AboutComponent },
    */
 ];
