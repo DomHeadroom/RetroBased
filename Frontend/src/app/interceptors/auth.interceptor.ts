b6212ed190cc09c58a0440000ec97858fa53e782
DomHeadroom <domefemia@protonmail.com>
2024-12-06 15:46:45 +0100

    Aggiustato auth interceptor, inserito label buy form, fixata scrollbal buy form

--- Frontend/src/app/interceptors/auth.interceptor.ts
+++ Frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,14 +1,19 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('authToken');
-  if (token) {
-    const clonedRequest = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return next(clonedRequest);
+  const isPublicRequest = req.url.includes('/public');
+
+  if (!isPublicRequest) {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      const clonedRequest = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return next(clonedRequest);
+    }
   }
+
   return next(req);
 };
